23078253c7a6379fb0299b856c96c240fd5ea502
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-04-20 20:50:08 +0200

    funktionierende Bildanalyse 1

--- V27_Zeeman_Effekt/1a_bildanalyse.py
+++ V27_Zeeman_Effekt/1a_bildanalyse.py
@@ -7,27 +7,28 @@ import scipy.signal
 img = imread('IMG_0021_rotated.jpg')
 
 # print("img", img)
-# sum colors
+# summiere Farbkanäle
 sums1 = img.sum(axis=2)
 # print("sums1", sums1)
-# sum along its vertical axis
+# summiere entlang der vertikalen Achse
 sums = sums1.sum(axis=0)
 # print(sums)
 
 min_height = 0.5 * max(sums)
-# min_height = 0
 peaks, _ = sp.signal.find_peaks(sums, height=min_height)
-# print(peaks)
 
 peak_dists = np.diff(peaks)
 print(peak_dists)
 print(peak_dists.mean())
 
 fig, ax = plt.subplots()
-ax.imshow(img)
-# ax.imshow(img, extent=[0, 4000, 0, 2248])
+# ax.imshow(img)
+ax.imshow(img, extent=[0, 4000, 0, 2248])
+# origin='lower' dreht Achsen und Bild
 x = np.array(range(len(sums)))
 displaysums = sums / max(sums) * sums1.shape[0]
 ax.plot(x, displaysums)
-ax.plot(peaks, displaysums[peaks], 'x')
+# ax.plot(peaks, displaysums[peaks], 'x')
+for peak in peaks:
+    ax.axvline(x=peak, color='r', alpha=0.25)
 plt.show()
