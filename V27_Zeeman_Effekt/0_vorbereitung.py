7283be7db40f9418aa5cfad85bae8b94901f9c19
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-04-26 11:22:33 +0200

    …

--- V27_Zeeman_Effekt/0_vorbereitung.py
+++ V27_Zeeman_Effekt/0_vorbereitung.py
@@ -34,15 +34,16 @@ def calc_A(L, n, λ):
 
 # █ 6. Dispersionsgebiet und spektrale Auflösung der Messapparatur
 
-
 DATA = [
     {
         'λ': ureg('643.8 nm'),
         'n': 1.4567,
+        'minmax': (-1, 1),
     },
     {
         'λ': ureg('480.0 nm'),
         'n': 1.4635,
+        'minmax': (-2, 2),
     },
 ]
 
@@ -50,8 +51,8 @@ d = ureg('4 mm')  # Dicke der LG-Platte
 L = ureg('120 mm')  # Länge der LG-Platte
 
 for data in DATA:
-    Δλ_D = calc_Δλ_D(d, **data)
-    A = calc_A(L, **data)
+    Δλ_D = calc_Δλ_D(d, λ=data['λ'], n=data['n'])
+    A = calc_A(L, λ=data['λ'], n=data['n'])
     print(f"→ {data['λ']:.1f}:")
     print(f"Δλ_D: {Δλ_D:.2f}")
     print(f"A: {A:.2f}")
@@ -63,36 +64,31 @@ def calc_E0(λ):
     return ureg.h * ureg.c / λ
 
 
-def p(E0, delta_λ, min_, max_):
+def calc_p(E0, delta_λ, min_, max_):
     return (E0 * delta_λ * (max_+min_) - ureg.h * ureg.c * (min_-max_)) / (delta_λ * ureg.mu_B * max_ * min_)
 
 
-def q(E0, min_, max_):
+def calc_q(E0, min_, max_):
     return E0**2/(ureg.mu_B**2 * max_ * min_)
 
 
-def B_plus(p, q):
+def calc_B_plus(p, q):
     return -p/2 + np.sqrt((p/2)**2-q)
 
 
-def B_minus(p, q):
+def calc_B_minus(p, q):
     return -p/2-np.sqrt((p/2)**2-q)
 
-
-max1 = 1
-min1 = -1
-max2 = 2
-min2 = -2
-
 for data in DATA:
+    min_, max_ = data['minmax']
     print(f"→ {data['λ']:.1f}:")
     E0 = calc_E0(data['λ'])
     print(f'E0: {E0.to("eV"):.2f}')
-    p1 = p(E0, calc_Δλ_D(d, data['n'], data['λ']), min1, max1)
-    print(f'p1: {p1:.2f}')
-    q1 = q(E0, min1, max1)
-    print(f'q1: {q1:.2f}')
-    B1p = B_plus(p1, q1)
-    print(f'B1p: {B1p.to("T"):.3f}')
-    B1m = B_minus(p1, q1)
-    print(f'B1m: {B1m.to("T"):.3f}')
+    p = calc_p(E0, calc_Δλ_D(d, data['n'], data['λ']), min_, max_)
+    print(f'p: {p:.2f}')
+    q = calc_q(E0, min_, max_)
+    print(f'q: {q:.2f}')
+    B_plus = calc_B_plus(p, q)
+    print(f'B_plus: {B_plus.to("T"):.3f}')
+    B_minus = calc_B_minus(p, q)
+    print(f'B_minus: {B_minus.to("T"):.3f}')
