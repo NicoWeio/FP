a7b1f185b9791395c6554fe8af191403ee406d1d
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-02 22:22:09 +0200

    [WIP] Der Polarisations-Fit will noch nicht

--- V61_HeNe_Laser/4_polarisation.py
+++ V61_HeNe_Laser/4_polarisation.py
@@ -20,21 +20,23 @@ def calc_I(
 I *= ureg.microwatt
 
 # Fit berechnen
-params = tools.pint_curve_fit(calc_I, ɑ, I, (ureg.deg, ureg.microwatt))
+# params = tools.pint_curve_fit(calc_I, ɑ, I, (ureg.deg, ureg.microwatt)) # zu niedrig…
 # params = tools.pint_curve_fit(calc_I, ɑ, I, (1*ureg.deg, 1*ureg.microwatt), p0=(90 * ureg.deg, max(I)))
-# params = tools.pint_curve_fit(calc_I, ɑ, I, (ureg.deg, ureg.microwatt), p0=(ureg('90 °'), max(I)))
+params = tools.pint_curve_fit(calc_I, ɑ, I, (1*ureg.deg, 1*ureg.microwatt), p0=(0 * ureg.deg, max(I)))
+params = tools.pint_curve_fit(calc_I, ɑ, I*1.5, (ureg.deg, ureg.microwatt))  # ACHTUNG: Fake-Daten
 print(f"params: {params}")
 nominal_params = [tools.nominal_value(p) for p in params]
 print(f"nominal_params: {nominal_params}")
-ɑ_linspace = tools.linspace(ureg('0 °'), ureg('360 °'))
+ɑ_linspace = tools.linspace(ureg('0 °'), ureg('360 °'), 500)
 
 
 with tools.plot_context(plt, '°', 'microwatt', r'\alpha', 'I') as plt2:
     plt2.plot(ɑ, I, '--o', zorder=5, label='Messwerte')
     plt2.plot(ɑ_linspace, calc_I(ɑ_linspace, *nominal_params), label='Fit')
 
+plt.gca().xaxis.set_ticks(np.arange(0, 360 * 9/8, 360/8))
 plt.grid()
 plt.legend()
 plt.tight_layout()
-plt.savefig('build/plt/4_polarisation.pdf')
-# plt.show()
+# plt.savefig('build/plt/4_polarisation.pdf')
+plt.show()
