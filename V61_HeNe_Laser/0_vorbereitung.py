cc8447c01adc55df65c902cb874e3ff86a039787
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-08 21:55:27 +0200

    nicerer Plot…

--- V61_HeNe_Laser/0_vorbereitung.py
+++ V61_HeNe_Laser/0_vorbereitung.py
@@ -24,9 +24,12 @@ MIRRORS = [
         'r1': ureg('1400 mm'),
         'r2': np.infty * ureg.mm,
     },
+    {
+        'r1': np.infty * ureg.mm,
+        'r2': np.infty * ureg.mm,
+    },
 ]
 
-# L = np.arange(-0.5, 3, 0.01) * ureg.m
 L = np.arange(0, 3 + 0.01, 0.01) * ureg.m
 
 
@@ -35,11 +38,19 @@ for num, mirror in enumerate(MIRRORS):
     g1 = calc_g(L, r1)
     g2 = calc_g(L, r2)
 
+    def fmt_r(r):
+        if r == np.infty * ureg.mm:
+            return '∞'
+        else:
+            # return f"{r.to('m').m:.1f}"
+            return r'\SI{' f"{r.to('m').m:.1f}" r'}{\meter}'
+
     with tools.plot_context(plt, 'm', 'dimensionless', 'L', 'g_1 \cdot g_2') as plt2:
-        plt2.plot(L, g1*g2, label=f"Anordnung {num} ($r_1 = {r1.to('m').m:.1f}, r_2 = {r2.to('m').m:.1f}$)")
+        plt2.plot(L, g1*g2, label=f"$r_1 = {fmt_r(r1)}$, $r_2 = {fmt_r(r2)}$")
 
 
 plt.fill_between(L, 0, 1, color='grey', alpha=0.5, label='stabil')
+plt.xlim(0, 3)
 plt.grid()
 plt.legend()
 plt.tight_layout()
