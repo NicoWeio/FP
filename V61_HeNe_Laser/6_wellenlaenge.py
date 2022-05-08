a9427a882adc7ff66c5670de0033c496892f27bf
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-08 12:05:59 +0200

    Code für alle Wellenlängen

--- V61_HeNe_Laser/6_wellenlaenge.py
+++ V61_HeNe_Laser/6_wellenlaenge.py
@@ -2,28 +2,51 @@ import tools
 import matplotlib.pyplot as plt
 import numpy as np
 import pint
+import rich
+from rich.console import Console
 ureg = pint.UnitRegistry()
 ureg.setup_matplotlib()
+console = Console()
 
-k, a_k = np.genfromtxt('dat/6_wellenlaenge_100.csv', delimiter=',', skip_header=1, unpack=True)
-a_k *= ureg.cm  # Distanz vom Hauptmaximum
-
-# https://www.leifiphysik.de/optik/beugung-und-interferenz/grundwissen/vielfachspalt-und-gitter
-
-d = 1 / ureg('100 / mm')  # Spaltabstand
-e = ureg('90.5 cm')  # Abstand Gitter—Schirm
+DATA = [
+    {
+        'g': ureg('100 / mm'),  # Spalte pro Längeneinheit
+        'e': ureg('90.5 cm'),  # Abstand Gitter—Schirm
+    },
+    {
+        'g': ureg('600 / mm'),
+        'e': ureg('90.5 cm'),
+    },
+    {
+        'g': ureg('80 / mm'),
+        'e': ureg('63.0 cm'),
+    },
+    {
+        'g': ureg('1200 / mm'),
+        'e': ureg('33.0 cm'),
+    },
+]
 λ_lit = ureg('632.816 nm')  # theoretische Wellenlänge
 
 
-# TODO: exakte Formel
+for data in DATA:
+    console.rule(f"{data['g']}")
+    k, a_k = np.genfromtxt(f"dat/6_wellenlaenge_{data['g'].m:.0f}.csv", delimiter=',', skip_header=1, unpack=True)
+    a_k *= ureg.cm  # Distanz vom Hauptmaximum
+    print(f"({len(a_k)} Werte)")
 
-# Näherung:
+    # https://www.leifiphysik.de/optik/beugung-und-interferenz/grundwissen/vielfachspalt-und-gitter
 
-# 0-Werte rauswerfen
-a_k = a_k[k != 0]
-k = k[k != 0]
+    d = 1 / data['g']  # Spaltabstand
+    e = ureg('90.5 cm')  # Abstand Gitter—Schirm
 
-all_λ = d * a_k / (k * e)
-λ = abs(all_λ).mean().to('nm')
-print(f"λ: {λ}")
-print(tools.fmt_compare_to_ref(λ, λ_lit))
+    # 0-Werte rauswerfen
+    a_k = a_k[k != 0]
+    k = k[k != 0]
+
+    # TODO: exakte Formel
+    # Näherung:
+
+    all_λ = d * a_k / (k * e)
+    λ = abs(all_λ).mean().to('nm')
+    print(tools.fmt_compare_to_ref(λ, λ_lit))
