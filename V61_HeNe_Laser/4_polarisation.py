794c54e248a8c6f962b2f22125c538b23140fba9
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-08 16:51:38 +0200

    Polarisation: Offset-Parameter hinzugefügt

--- V61_HeNe_Laser/4_polarisation.py
+++ V61_HeNe_Laser/4_polarisation.py
@@ -8,11 +8,11 @@ ureg.setup_matplotlib()
 
 def calc_I(
     ɑ,  # Parameter
-    ɑ0, I0,  # Konstanten
+    ɑ0, I_max, I_0,  # Konstanten
 ):
     """TEM₀₀: Theoretische Intensität in Abhängigkeit vom Abstand r von der Modenmitte."""
     # return I0 * np.cos(ɑ + ɑ0)**2
-    return I0 * np.sin(ɑ + ɑ0)**2
+    return I_max * np.sin(ɑ + ɑ0)**2 + I_0
 
 
 ɑ, I = np.genfromtxt('dat/4_polarisation.csv', delimiter=',', skip_header=1, unpack=True)
@@ -31,7 +31,7 @@ print(f"nominal_params: {nominal_params}")
 
 
 with tools.plot_context(plt, '°', 'microwatt', r'\alpha', 'I') as plt2:
-    plt2.plot(ɑ, I, '--o', zorder=5, label='Messwerte')
+    plt2.plot(ɑ, I, 'x', zorder=5, label='Messwerte')
     plt2.plot(ɑ_linspace, calc_I(ɑ_linspace, *nominal_params), label='Fit')
 
 plt.gca().xaxis.set_ticks(np.arange(0, 360 * 9/8, 360/8))
