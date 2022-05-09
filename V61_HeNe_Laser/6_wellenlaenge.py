f8bb36e28fa43a45d93cdf694eec3a2d2af47490
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-09 12:52:55 +0200

    Stuff zur Wellenlänge-Auswertung

--- V61_HeNe_Laser/6_wellenlaenge.py
+++ V61_HeNe_Laser/6_wellenlaenge.py
@@ -28,6 +28,8 @@ DATA = [
 ]
 λ_lit = ureg('632.816 nm')  # theoretische Wellenlänge
 
+global_λ = []
+global_all_λ = []
 
 for data in DATA:
     console.rule(f"{data['g']}")
@@ -39,6 +41,19 @@ for data in DATA:
     a_k = a_k[k != 0]
     k = k[k != 0]
 
+    # # TEST: Symmetrische Messung simulieren (setzt vorzeichenbehaftetes k voraus)
+    # largest_common_k = min(max(k), abs(min(k)))
+    # # Hinweis ausgeben
+    # discarded_k = k[abs(k) > largest_common_k]
+    # if len(discarded_k):
+    #     print(f"{len(discarded_k)} k-Werte wurden entfernt: {discarded_k}")
+    # # Darüber hinausgehende Werte abschneiden
+    # a_k = a_k[abs(k) <= largest_common_k]
+    # k = k[abs(k) <= largest_common_k]
+
+    # |k|
+    k = np.abs(k)
+
     # https://www.leifiphysik.de/optik/beugung-und-interferenz/grundwissen/vielfachspalt-und-gitter
     d = 1 / data['g']  # Spaltabstand
     e = ureg('90.5 cm')  # Abstand Gitter—Schirm
@@ -47,6 +62,17 @@ for data in DATA:
     # all_λ = d * a_k / (k * e)
     # exakte Formel:
     all_λ = d * a_k / (k * np.sqrt(e**2 + a_k**2))
+    print(f"all_λ = {all_λ.to('nm'):.1f}")
 
     λ = tools.ufloat_from_list(abs(all_λ).to('nm'))
     print(tools.fmt_compare_to_ref(λ, λ_lit))
+    global_λ.append(λ)
+    global_all_λ.extend(all_λ)
+
+console.rule("Mittelwert A")
+λ_mean = tools.pintify(global_λ).to('nm').m.mean() * ureg('nm')  # Workaround: die einzelnen λ haben bereits eine Unsicherheit
+print(tools.fmt_compare_to_ref(λ_mean, λ_lit))
+
+console.rule("Mittelwert B")
+λ_mean = tools.pintify(global_all_λ).to('nm').m.mean() * ureg('nm')
+print(tools.fmt_compare_to_ref(λ_mean, λ_lit))
