616399086c82736aed6a787ddc6e834bc3b9dd68
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-12 21:56:17 +0200

    Frequenzspekrum: Tabelle! 🎉

--- V61_HeNe_Laser/5_frequenzspektrum.py
+++ V61_HeNe_Laser/5_frequenzspektrum.py
@@ -10,30 +10,40 @@ ureg.setup_matplotlib()
 console = Console()
 
 L = ureg('162.9 cm')
-# L = ureg('50 cm') # TODO: TEST!
 
 f, I = np.genfromtxt('dat/5_frequenzspektrum.csv', delimiter=',', skip_header=1, unpack=True)
 f *= ureg('MHz')
 I *= ureg('dBm')
 
-# Tabelle erzeugen
-generate_table_pint(
-    f'build/tab/5_frequenzspektrum.tex',
-    ('f', ureg.MHz, f, 0),
-    ('I', ureg.dBm, I, 1),
-    ('I', ureg.microwatt, I, 1),
-)
-
 
 Δf_theo = (ureg.c / (2 * L)).to('MHz')
+print(f'Δf_theo = {Δf_theo:.2f}')
 
-f_theo = Δf_theo * np.arange(1, (max(f) // Δf_theo).m + 1)
+f_theo = Δf_theo * np.arange(0, (max(f) // Δf_theo).m + 1)
 print(f'f_mess = {f:.2f}')
 print(f'f_theo = {f_theo:.2f}')
 
+
+data = []
 for single_f in f:
-    best_n = np.argmin(np.abs(f_theo - single_f))  # off-by-one!
-    print(f'Mess: {single_f} ↔ Theo: {f_theo[best_n]} (n={best_n+1}) → Abweichung: {(f_theo[best_n] - single_f):.2f}')
+    best_n = np.argmin(np.abs(f_theo - single_f))
+    best_f_theo = f_theo[best_n]
+    deviation = f_theo[best_n] - single_f
+    data.append((best_n, best_f_theo, deviation))
+    print(f'Mess: {single_f:.2f} ↔ Theo: {best_f_theo:.2f} (n={best_n}) → Abweichung: {deviation:.2f}, rel. Abw.: {(deviation / best_f_theo).m:.2%}')
+
+best_n, best_f_theo, deviation = zip(*data)
+
+# Tabelle erzeugen
+generate_table_pint(
+    f'build/tab/5_frequenzspektrum.tex',
+    ('n', ureg.dimensionless, best_n * ureg.dimensionless, 0),
+    ('f', ureg.MHz, f, 0),
+    ('I', ureg.dBm, I, 1),
+    # ('I', ureg.microwatt, I, 1),
+    (r'f_\text{theo}', ureg.MHz, best_f_theo, 1),
+    (r'(f_\text{theo} - f)', ureg.MHz, deviation, 1),
+)
 
 
 # Ansatz siehe Mampfzwerg. Keine schönen Ergebnisse…
