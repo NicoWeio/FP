58255c86bcd636a1cf0a670d57433a7b77157b76
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-17 21:39:00 +0200

    …

--- V21_Optisches_Pumpen/auswertung.py
+++ V21_Optisches_Pumpen/auswertung.py
@@ -26,39 +26,40 @@ def calc_kernspin(g_f, J):
 def quad_zeemann(g_f, B, E_HFS, m_f):
     return g_f**2 * (ureg.e * ureg.h/(4 * np.pi * ureg.m_e))**2 * B**2 * (1 - 2 * m_f)/E_HFS
 
-
-# Werte auslesen. rf_freq zunächst in kHz | U1_hor, U1_sweep, U2_hor, U2_sweep alles in V und hor hat 13.8V offset
-
+# Daten einlesen
 rf_freq, U1_hor, U1_sweep, U2_hor, U2_sweep = np.genfromtxt("data.txt", unpack=True)
-rf_freq *= ureg.Hz
+rf_freq *= ureg.Hz #TODO: Einheit korrekt?
 U1_hor *= ureg.V
 U2_hor *= ureg.V
 U1_sweep *= ureg.V
 U2_sweep *= ureg.V
+
+# Offset korrigieren
+U1_hor -= ureg('13.8 V')
+U2_hor -= ureg('13.8 V')
+
+# Unsicherheiten einfügen
 # U1_hor = unp.uarray(U1_hor, 0.02)
 # U2_hor = unp.uarray(U2_hor, 0.02)
 # U1_sweep = unp.uarray(U1_sweep, 0.01)
 # U2_sweep = unp.uarray(U2_sweep, 0.01)
 
 
-#Horizontalspule:
-r_hor = ureg('0.1579 m') # Radius
-N_hor = 154 # Windungen
-R_hor = 10/3 * ureg.ohm # Widerstand
-
+# █ Daten zu den Spulen [Versuchsanleitung]
 #Sweepspule:
-r_sweep = ureg('0.1639 m')
-N_sweep = 11
-R_sweep = 10 * ureg.ohm
+r_sweep = ureg('16.39 cm') # Radius
+N_sweep = 11 # Windungen
+R_sweep = 10 * ureg.ohm # Widerstand
+
+#Horizontalspule:
+r_hor = ureg('15.79 cm')
+N_hor = 154
+R_hor = 10/3 * ureg.ohm
 
 #Vertikalspule:
-r_ver = ureg('0.11735 m')
+r_ver = ureg('11.735 cm')
 N_ver = 20
 
-
-# TODO: Was passiert hier!?
-U1_hor -= ureg('13.8 V')
-U2_hor -= ureg('13.8 V')
 
 I1_hor = U1_hor/R_hor
 I2_hor = U2_hor/R_hor
@@ -87,8 +88,8 @@ print(f"Dip1: {max(B1_ges), np.argmax(B1_ges)}, Dip2: {max(B2_ges), np.argmax(B2
 params_87 = tools.linregress(rf_freq, B1_ges)
 params_85 = tools.linregress(rf_freq, B2_ges)
 
-print(f"m, b von Rb-85: {params_85}")
-print(f"m, b von Rb-87: {params_87}")
+print(f"m, b von 85Rb: {params_85}")
+print(f"m, b von 87Rb: {params_87}")
 
 # Kernspin aus Steigung berechnen
 
@@ -108,16 +109,16 @@ plt.legend()
 
 g_f1 = g(params_87[0])
 g_f2 = g(params_85[0])
-print(f"Rb-87: g_f={g_f1}")
-print(f"Rb-85: g_f={g_f2}")
+print(f"87Rb: g_f={g_f1}")
+print(f"85Rb: g_f={g_f2}")
 
 # ---
 
 I_1 = calc_kernspin(g_f1, 1/2)
 I_2 = calc_kernspin(g_f2, 1/2)
 
-print(f"Rb-87: I={I_1}")
-print(f"Rb-85: I={I_2}")
+print(f"87Rb: I={I_1}")
+print(f"85Rb: I={I_2}")
 
 
 # Erdmagnetfeld in vertikaler und horizontaler Richtung bestimmen
@@ -133,22 +134,28 @@ print(B_vert)
 
 
 B_hor = (params_87[1] + params_85[1])/2
-print("Horizontale Magnetfeldkomponente in µT aus y-Achsenabschnitt des lin. Zusammenhangs zwischen B-Feld unf RF-Frequenz")
+print("Horizontale Magnetfeldkomponente in µT aus y-Achsenabschnitt des lin. Zusammenhangs zwischen B-Feld und RF-Frequenz")
 print(B_hor)
 
 
 # Vergleichswerte:
 # horizontale Magntefledkomponente ~20 µT; vertikale Magnetfeldkomponente ~40 µT
 
-# Das Verhältnis der beiden Isotope lässt sich anhand des Amplitudenverhältnis der beiden Dips (1. Dip Rb-87 und 2. Dip Rb-85) ablesen.
-# Es beträgt Rb-87 = 0.5 * Rb-85
+# Das Verhältnis der beiden Isotope lässt sich anhand des Amplitudenverhältnis der beiden Dips (1. Dip 87Rb und 2. Dip 85Rb) ablesen.
+# Es beträgt 87Rb = 0.5 * 85Rb
 
 # Abschätzung des quadratischen Zeemann-Effekts:
 
+
+# HFS = Hyperfeinstruktur
+# Hyperfeinstrukturaufspaltung des Grundzustandes [Versuchsanleitung, Messprogramm h)]:
 E_HFS_87 = 4.53e-24
 E_HFS_85 = 2.01e-24
+
+# QZ = quadratischer Zeeman-Effekt
+# TODO: Argmax (9) nicht hardcoden ↓
 E_QZ_87 = quad_zeemann(g_f1, B1_ges[9], E_HFS_87, 1)
 E_QZ_85 = quad_zeemann(g_f2, B2_ges[9], E_HFS_85, 1)
 
 #print("Aufspaltung durch den quadratischen Zeemann-Effekt bei höheren Magnetfeldstärken")
-print(f"Rb-87:{abs(E_QZ_87)} J bei {max(B1_ges)}, Rb-85: {abs(E_QZ_85)} J bei {max(B2_ges)}")
+print(f"87Rb:{abs(E_QZ_87)} J bei {max(B1_ges)}, 85Rb: {abs(E_QZ_85)} J bei {max(B2_ges)}")
