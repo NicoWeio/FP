cfea897daf74f506c020523d2afe272c58c94fca
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-17 22:28:20 +0200

    …

--- V21_Optisches_Pumpen/auswertung.py
+++ V21_Optisches_Pumpen/auswertung.py
@@ -1,26 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pint
+from rich.console import Console
 import tools
 import uncertainties.unumpy as unp
 ureg = pint.UnitRegistry()
+ureg.setup_matplotlib()
+console = Console()
 
 
 def calc_B_helmholtz(r, N, I):
     B = 8 * ureg.mu_0 * I * N / (np.sqrt(125) * r)
-    return B
+    return B.to('µT')
 
 
 def calc_g(m):
-    return 4 * np.pi * ureg.m_e / (m * ureg.e)
+    g = 4 * np.pi * ureg.m_e / (m * ureg.e)
+    return g.to('dimensionless')
 
 
-def calc_kernspin(g_f, J):
-    return J * (2/g_f - 1)
+def calc_kernspin(g_F, J):
+    return J * (2/g_F - 1)
 
 
-def calc_quad_zeemann(g_f, B, E_HFS, m_f):
-    return g_f**2 * (ureg.e * ureg.h/(4 * np.pi * ureg.m_e))**2 * B**2 * (1 - 2 * m_f)/E_HFS
+def calc_quad_zeemann(g_F, B, E_HFS, m_f):
+    E = g_F**2 * (ureg.e * ureg.h/(4 * np.pi * ureg.m_e))**2 * B**2 * (1 - 2 * m_f)/E_HFS
+    return E.to('J')
 
 
 # █ Daten einlesen
@@ -70,21 +75,27 @@ B1_ges = B1_hor + B1_sweep
 B2_hor = calc_B_helmholtz(r_hor, N_hor, I2_hor)
 B2_sweep = calc_B_helmholtz(r_sweep, N_sweep, I2_sweep)
 B2_ges = B2_hor + B2_sweep
-print(B2_ges)
-#print("Gesamte horizontale Magnetfeldstärke in Tesla")
-print(f"Dip1: {max(B1_ges), np.argmax(B1_ges)}, Dip2: {max(B2_ges), np.argmax(B2_ges)}")
 
 
-# ---
+dip1 = np.argmax(B1_ges)
+dip2 = np.argmax(B2_ges)
+print(f"Dip1: {max(B1_ges), dip1}, Dip2: {max(B2_ges), dip2}")
+
+# TODO: Plot, weil ich nicht verstehe, was hier abgeht.
+plt.stackplot(rf_freq, B1_hor, B1_sweep, labels=['Horizontalspule', 'Sweepspule'])
+plt.axvline(rf_freq[dip1], color='k', linestyle='--', label='Dip-Dings')
+# plt.stackplot(rf_freq, B2_hor, B2_sweep, labels=['Horizontalspule', 'Sweepspule'])
+# plt.axvline(rf_freq[dip2], color='k', linestyle='--', label='Dip-Dings')
+plt.legend()
+# plt.show()
 
 
+console.rule("lineare Regression")
 params_87 = tools.linregress(rf_freq, B1_ges)
 params_85 = tools.linregress(rf_freq, B2_ges)
-
-print(f"85Rb: (m, b)={params_85}")
 print(f"87Rb: (m, b)={params_87}")
+print(f"85Rb: (m, b)={params_85}")
 
-# Kernspin aus Steigung berechnen
 
 rf_freq_linspace = tools.linspace(*tools.bounds(rf_freq))
 with tools.plot_context(plt, 'kHz', 'µT', 'f', 'B') as plt2:
@@ -98,57 +109,53 @@ plt.legend()
 # plt.show()
 
 
-# g-Faktoren berechnen [d) in der Versuchsanleitung]
+console.rule("g-Faktoren [d) in der Versuchsanleitung]")
+g_F_87 = calc_g(params_87[0])
+g_F_85 = calc_g(params_85[0])
+print(f"87Rb: g_F={g_F_87}")
+print(f"85Rb: g_F={g_F_85}")
 
-g_f1 = calc_g(params_87[0])
-g_f2 = calc_g(params_85[0])
-print(f"87Rb: g_f={g_f1}")
-print(f"85Rb: g_f={g_f2}")
-
-# ---
-
-I_1 = calc_kernspin(g_f1, J=1/2)
-I_2 = calc_kernspin(g_f2, J=1/2)
-
+console.rule("Kernspins [e) in der Versuchsanleitung]")
+I_1 = calc_kernspin(g_F_87, J=1/2)
+I_2 = calc_kernspin(g_F_85, J=1/2)
 print(f"87Rb: I={I_1}")
 print(f"85Rb: I={I_2}")
 
 
-# Erdmagnetfeld in vertikaler und horizontaler Richtung bestimmen
-
-U_vert = unp.uarray(2.26, 0.01) * ureg.V
+console.rule("Erdmagnetfeld: vertikal")
+# Vertikale Magnetfeldkomponente in µT aus Spannung der vertikalen Spule
+U_vert = unp.uarray(2.26, 0.01) * ureg.V  # TODO: eigene Werte!
 I_vert = U_vert/R_sweep
 B_vert = calc_B_helmholtz(r_ver, N_ver, I_vert)
-print("Vertikale Magnetfeldkomponente in µT aus Spannung der vertikalen Spule")
-print(B_vert)
+print(f"B_vert = {B_vert}")
+print(tools.fmt_compare_to_ref(B_vert, ureg('40 µT')))  # TODO: Quelle unbekannt
 
 
-# ---
-
-
+console.rule("Erdmagnetfeld: horizontal")
+# Horizontale Magnetfeldkomponente aus y-Achsenabschnitt des lin. Zusammenhangs zwischen B-Feld und RF-Frequenz
 B_hor = (params_87[1] + params_85[1])/2
-print("Horizontale Magnetfeldkomponente in µT aus y-Achsenabschnitt des lin. Zusammenhangs zwischen B-Feld und RF-Frequenz")
-print(B_hor)
+print(f"B_hor = {B_hor}")
+print(tools.fmt_compare_to_ref(B_hor, ureg('20 µT')))  # TODO: Quelle unbekannt
 
 
-# Vergleichswerte:
-# horizontale Magntefledkomponente ~20 µT; vertikale Magnetfeldkomponente ~40 µT
-
+console.rule("Isotopenverhältnis")
 # Das Verhältnis der beiden Isotope lässt sich anhand des Amplitudenverhältnis der beiden Dips (1. Dip 87Rb und 2. Dip 85Rb) ablesen.
 # Es beträgt 87Rb = 0.5 * 85Rb
+# print(f"Isotopenverhältnis…", max(B1_ges) / max(B2_ges)) # random guess
+print("[…]")
 
-# Abschätzung des quadratischen Zeemann-Effekts:
 
+console.rule("Abschätzung des quadratischen Zeemann-Effekts")
 
 # HFS = Hyperfeinstruktur
 # Hyperfeinstrukturaufspaltung des Grundzustandes [Versuchsanleitung, Messprogramm h)]:
-E_HFS_87 = 4.53e-24
-E_HFS_85 = 2.01e-24
+E_HFS_87 = 4.53e-24 * ureg.J
+E_HFS_85 = 2.01e-24 * ureg.J
 
 # QZ = quadratischer Zeeman-Effekt
-# TODO: Argmax (9) nicht hardcoden ↓
-E_QZ_87 = calc_quad_zeemann(g_f1, B1_ges[9], E_HFS_87, 1)
-E_QZ_85 = calc_quad_zeemann(g_f2, B2_ges[9], E_HFS_85, 1)
+E_QZ_87 = calc_quad_zeemann(g_F_87, max(B1_ges), E_HFS_87, 1)
+E_QZ_85 = calc_quad_zeemann(g_F_85, max(B2_ges), E_HFS_85, 1)
 
 #print("Aufspaltung durch den quadratischen Zeemann-Effekt bei höheren Magnetfeldstärken")
-print(f"87Rb:{abs(E_QZ_87)} J bei {max(B1_ges)}, 85Rb: {abs(E_QZ_85)} J bei {max(B2_ges)}")
+print(f"87Rb: {abs(E_QZ_87)} bei {max(B1_ges):.2f}")
+print(f"85Rb: {abs(E_QZ_85)} bei {max(B2_ges):.2f}")
