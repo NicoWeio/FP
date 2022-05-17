19240a392651446d4d32d67463a6620d0933ca94
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-17 15:45:30 +0200

    Pintifizierung, Pt. 1

--- V21_Optisches_Pumpen/auswertung.py
+++ V21_Optisches_Pumpen/auswertung.py
@@ -1,6 +1,3 @@
-# In[1]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as const
@@ -8,16 +5,19 @@ import uncertainties.unumpy as unp
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 from scipy.optimize import curve_fit
 
+import pint
+ureg = pint.UnitRegistry()
 
-# In[2]:
+# ---
 
 
 def linear(x, m, b):
     return m*x+b
 
 
-def b_helm(r, N, I):
-    return 8 * const.mu_0 * I * N / (np.sqrt(125) * r)
+def b_helmholtz(r, N, I):
+    B = 8 * ureg.mu_0 * I * N / (np.sqrt(125) * r)
+    return B.to('µT')
 
 
 def g(m):
@@ -34,43 +34,44 @@ def quad_zeemann(g_f, B, E_HFS, m_f):
 
 # Werte auslesen. rf_freq zunächst in kHz | U1_hor, U1_sweep, U2_hor, U2_sweep alles in V und hor hat 13.8V offset
 
-# In[3]:
-
-
 rf_freq, U1_hor, U1_sweep, U2_hor, U2_sweep = np.genfromtxt("data.txt", unpack=True)
-rf_freq = rf_freq * 1000  # Nun von kHz in Hz umgerechnet
-U1_hor = unp.uarray(U1_hor, 0.02)
-U2_hor = unp.uarray(U2_hor, 0.02)
-U1_sweep = unp.uarray(U1_sweep, 0.01)
-U2_sweep = unp.uarray(U2_sweep, 0.01)
+rf_freq *= ureg.Hz
+U1_hor *= ureg.V
+U2_hor *= ureg.V
+U1_sweep *= ureg.V
+U2_sweep *= ureg.V
+# U1_hor = unp.uarray(U1_hor, 0.02)
+# U2_hor = unp.uarray(U2_hor, 0.02)
+# U1_sweep = unp.uarray(U1_sweep, 0.01)
+# U2_sweep = unp.uarray(U2_sweep, 0.01)
 
-#print(U1_hor[1])
-#print(noms(U1_hor)[1])
-#print(stds(U1_hor)[1])
 
-
-# In[4]:
+# ---
 
 
 #Horizontalspule:
-r_hor = 0.1579
-N_hor = 154
-R_hor = 10/3
+r_hor = ureg('0.1579 m') # Radius
+N_hor = 154 # Windungen
+R_hor = 10/3 * ureg.ohm # Widerstand
+
 #Sweepspule:
-r_sweep = 0.1639
+r_sweep = ureg('0.1639 m')
 N_sweep = 11
-R_sweep = 10
+R_sweep = 10 * ureg.ohm
+
 #Vertikalspule:
-r_ver = 0.11735
+r_ver = ureg('0.11735 m')
 N_ver = 20
 
 
-# In[5]:
+# ---
 
 
 #Spannungen in Volt
-U1_hor = U1_hor - 13.8
-U2_hor = U2_hor - 13.8
+# TODO: !?
+U1_hor -= ureg('13.8 V')
+U2_hor -= ureg('13.8 V')
+
 #Stromstärken in Ampere
 I1_hor = U1_hor/R_hor
 I2_hor = U2_hor/R_hor
@@ -78,22 +79,22 @@ I1_sweep = U1_sweep/R_sweep
 I2_sweep = U2_sweep/R_sweep
 
 
-# In[6]:
+# ---
 
 
-B1_hor = b_helm(r_hor, N_hor, I1_hor)
-B1_sweep = b_helm(r_sweep, N_sweep, I1_sweep)
+B1_hor = b_helmholtz(r_hor, N_hor, I1_hor)
+B1_sweep = b_helmholtz(r_sweep, N_sweep, I1_sweep)
 B1_ges = B1_hor + B1_sweep
 
-B2_hor = b_helm(r_hor, N_hor, I2_hor)
-B2_sweep = b_helm(r_sweep, N_sweep, I2_sweep)
+B2_hor = b_helmholtz(r_hor, N_hor, I2_hor)
+B2_sweep = b_helmholtz(r_sweep, N_sweep, I2_sweep)
 B2_ges = B2_hor + B2_sweep
 print(B2_ges)
 #print("Gesamte horizontale Magnetfeldstärke in Tesla")
 print(f"Dip1:{max(B1_ges), np.argmax(B1_ges)}, Dip2:{max(B2_ges), np.argmax(B2_ges)}")
 
 
-# In[7]:
+# ---
 
 
 params_1, cov_1 = curve_fit(linear, rf_freq, noms(B1_ges), sigma=stds(B1_ges))
@@ -109,7 +110,7 @@ print(f"m, b von Rb-87: {params_87}")
 
 # Kernspin aus Steigung berechnen
 
-# In[8]:
+# ---
 
 
 x = np.linspace(0, 1000000)
@@ -123,7 +124,7 @@ plt.legend(loc="best")
 plt.savefig("Rb_85_87.pdf")
 
 
-# In[9]:
+# ---
 
 
 #x = np.linspace(0,1000000)
@@ -136,7 +137,7 @@ plt.savefig("Rb_85_87.pdf")
 
 # g-Faktoren berechnen
 
-# In[10]:
+# ---
 
 
 g_f1 = g(unp.uarray(params_1[0], errors1[0]))
@@ -144,15 +145,10 @@ g_f2 = g(unp.uarray(params_2[0], errors2[0]))
 print(f"Rb-85: g_f={g_f2}")
 print(f"Rb-87: g_f={g_f1}")
 
-
-# In[11]:
-
+# ---
 
 I_1 = kernspin(g_f1, 1/2)
 I_2 = kernspin(g_f2, 1/2)
-
-
-# In[12]:
 
 print(f"Rb-85: I={I_2}")
 print(f"Rb-87: I={I_1}")
@@ -161,18 +157,15 @@ print(f"Rb-87: I={I_1}")
 # Erdmagnetfeld in vertikaler und horizontaler Richtung bestimmen
 #
 
-# In[13]:
-
-
 #Spannung in Volt
 U_vert = unp.uarray(2.26, 0.01)
 I_vert = U_vert/R_sweep
-B_vert = b_helm(r_ver, N_ver, I_vert)
+B_vert = b_helmholtz(r_ver, N_ver, I_vert)
 print("Vertikale Magnetfeldkomponente in muT aus Spannung der vertikalen Spule")
 print(B_vert * 1000000)
 
 
-# In[14]:
+# ---
 
 
 B_hor = (unp.uarray(params_1[1], errors1[1]) + unp.uarray(params_2[1], errors2[1]))/2
@@ -188,7 +181,7 @@ print(B_hor * 1000000)
 # Abschätzung des quadratischen Zeemann-Effekts:
 #
 
-# In[19]:
+# ---
 
 
 E_HFS_87 = 4.53 * 10**(-24)
@@ -198,6 +191,3 @@ E_QZ_85 = quad_zeemann(g_f2, B2_ges[9], E_HFS_85, 1)
 
 #print("Aufspaltung durch den quadratischen Zeemann-Effekt bei höheren Magnetfeldstärken")
 print(f"Rb-87:{abs(E_QZ_87)} J bei {max(B1_ges)*1000000} Mikrotesla, Rb-85: {abs(E_QZ_85)} J bei {max(B2_ges)*1000000} Mikrotesla")
-
-
-# In[ ]:
