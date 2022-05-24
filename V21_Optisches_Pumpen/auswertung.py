8d5b5e6e5df412a7ddf75f7e2eb940d11045f2a5
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-24 10:47:52 +0200

    …

--- V21_Optisches_Pumpen/auswertung.py
+++ V21_Optisches_Pumpen/auswertung.py
@@ -11,6 +11,7 @@ console = Console()
 
 
 def calc_B_helmholtz(r, N, I):
+    """Magnetfeld im Zentrum eines Helmholtz-Spulenpaars aus Radius `r`, Windungszahl `N` und Strom `I`"""
     B = 8 * ureg.mu_0 * I * N / (np.sqrt(125) * r)
     return B.to('µT')
 
@@ -47,26 +48,33 @@ U2_sweep *= ureg.V
 
 # █ Daten zu den Spulen [Versuchsanleitung]
 # Sweepspule:
-r_sweep = ureg('16.39 cm')  # Radius
-N_sweep = 11  # Windungen
-R_sweep = 10 * ureg.ohm  # Widerstand # [Quelle unbekannt]
-
+dat_sweep = {
+    'r': ureg('16.39 cm'),  # Radius
+    'N': 11,  # Windungen
+    'R': 10 * ureg.ohm,  # Widerstand # [Quelle unbekannt]
+}
 # Horizontalspule:
-r_hor = ureg('15.79 cm')
-N_hor = 154
-R_hor = 10/3 * ureg.ohm  # [Quelle unbekannt]
-
+dat_hor = {
+    'r': ureg('15.79 cm'),
+    'N': 154,
+    'R':  10/3 * ureg.ohm,  # [Quelle unbekannt]
+}
 # Vertikalspule:
-r_ver = ureg('11.735 cm')
-N_ver = 20
+dat_vert = {
+    'r': ureg('11.735 cm'),
+    'N': 20,
+}
 
-
-B1_sweep = calc_B_helmholtz(r_sweep, N_sweep, I=(U1_sweep / R_sweep))
-B1_hor = calc_B_helmholtz(r_hor, N_hor, I1_hor)
-B1_ges = B1_hor + B1_sweep
-B2_sweep = calc_B_helmholtz(r_sweep, N_sweep, I=(U2_sweep / R_sweep))
-B2_hor = calc_B_helmholtz(r_hor, N_hor, I2_hor)
-B2_ges = B2_hor + B2_sweep
+# Gesamt-Magnetfelder berechnen
+# (das Vertikalfeld ist zu 0 kompensiert)
+B1_ges = (  # Sweepspule + Horizontalspule
+    calc_B_helmholtz(dat_sweep['r'], dat_sweep['N'], I=(U1_sweep / dat_sweep['R'])) +
+    calc_B_helmholtz(dat_hor['r'], dat_hor['N'], I1_hor)
+)
+B2_ges = (  # Sweepspule + Horizontalspule
+    calc_B_helmholtz(dat_sweep['r'], dat_sweep['N'], I=(U2_sweep / dat_sweep['R'])) +
+    calc_B_helmholtz(dat_hor['r'], dat_hor['N'], I2_hor)
+)
 
 # █ Tabelle generieren
 # generate_table.generate_table_pint(
@@ -86,13 +94,13 @@ print(f"85Rb: (m, b)={params_85}")
 rf_freq_linspace = tools.linspace(*tools.bounds(rf_freq))
 plt.figure()
 with tools.plot_context(plt, 'kHz', 'µT', 'f', 'B') as plt2:
-    # TODO: Errorbars sollten erscheinen, sobald U eine Unsicherheit erhält.
     plt2.plot(rf_freq, B2_ges, 'x', zorder=5, label=r"Messwerte zu $^{85}$Rb")
-    plt2.plot(rf_freq_linspace, tools.nominal_values(params_85[0]*rf_freq_linspace + params_85[1]), label=r"Ausgleichsgerade zu $^{85}$Rb")
+    plt2.plot(rf_freq_linspace, tools.nominal_values(
+        params_85[0]*rf_freq_linspace + params_85[1]), label=r"Ausgleichsgerade zu $^{85}$Rb")
 
-    # TODO: Errorbars sollten erscheinen, sobald U eine Unsicherheit erhält.
     plt2.plot(rf_freq, B1_ges, 'x', zorder=5, label=r"Messwerte zu $^{87}$Rb")
-    plt2.plot(rf_freq_linspace, tools.nominal_values(params_87[0]*rf_freq_linspace + params_87[1]), label=r"Ausgleichsgerade zu $^{87}$Rb")
+    plt2.plot(rf_freq_linspace, tools.nominal_values(
+        params_87[0]*rf_freq_linspace + params_87[1]), label=r"Ausgleichsgerade zu $^{87}$Rb")
 plt.legend()
 plt.tight_layout()
 plt.savefig("build/plt/g_F.pdf")
@@ -101,46 +109,44 @@ plt.savefig("build/plt/g_F.pdf")
 # █ Berechnung
 g_F_87 = calc_g(params_87[0])
 g_F_85 = calc_g(params_85[0])
-print(f"87Rb:\n", tools.fmt_compare_to_ref(g_F_87, 1/2))
-print(f"85Rb:\n", tools.fmt_compare_to_ref(g_F_85, 1/3))
+print(tools.fmt_compare_to_ref(g_F_87, 1/2, name='g_F (87Rb)'))
+print(tools.fmt_compare_to_ref(g_F_85, 1/3, name='g_F (85Rb)'))
 
 console.rule("Kernspins [e) in der Versuchsanleitung]")
 I_1 = calc_kernspin(g_F_87, J=1/2)
 I_2 = calc_kernspin(g_F_85, J=1/2)
-print("87Rb:\n", tools.fmt_compare_to_ref(I_1, 1.5))
-print("85Rb:\n", tools.fmt_compare_to_ref(I_2, 2.5))
+print(tools.fmt_compare_to_ref(I_1, 1.5, name='I (87Rb)'))
+print(tools.fmt_compare_to_ref(I_2, 2.5, name='I (85Rb)'))
 
 
 console.rule("Erdmagnetfeld: vertikal")
 # Vertikale Magnetfeldkomponente in µT aus Spannung der vertikalen Spule
 U_vert = unp.uarray(2.26, 0.01) * ureg.V  # TODO: eigene Werte!
-B_vert = calc_B_helmholtz(r_ver, N_ver, I=(U_vert / R_sweep))
+B_vert = calc_B_helmholtz(dat_vert['r'], dat_vert['N'], I=(U_vert / dat_sweep['R'])) # TODO: warum dat_sweep?
 print(tools.fmt_compare_to_ref(B_vert, ureg('40 µT')))  # TODO: Quelle unbekannt
 
 
 console.rule("Erdmagnetfeld: horizontal")
 # Horizontale Magnetfeldkomponente aus y-Achsenabschnitt des lin. Zusammenhangs zwischen B-Feld und RF-Frequenz
-B_hor = (params_87[1] + params_85[1])/2 # Mittelwert aus den Achsenabschnitten der beiden Regressionsgeraden
+B_hor = (params_87[1] + params_85[1])/2  # Mittelwert aus den Achsenabschnitten der beiden Regressionsgeraden
 print(tools.fmt_compare_to_ref(B_hor, ureg('20 µT')))  # TODO: Quelle unbekannt
 
 
 console.rule("Isotopenverhältnis")
+# Idee: Peak-Tiefe ~ Anteil des Isotops
 # Das Verhältnis der beiden Isotope lässt sich anhand des Amplitudenverhältnis der beiden Dips (1. Dip 87Rb und 2. Dip 85Rb) ablesen.
-# Es beträgt 87Rb = 0.5 * 85Rb
-# print(f"Isotopenverhältnis…", max(B1_ges) / max(B2_ges)) # random guess
 print("[…]")
 
 
 console.rule("Abschätzung des quadratischen Zeemann-Effekts")
 
-# HFS = Hyperfeinstruktur
-# Hyperfeinstrukturaufspaltung des Grundzustandes [Versuchsanleitung, Messprogramm h)]:
+# Hyperfeinstrukturaufspaltung des Grundzustandes [Quelle: Versuchsanleitung, Messprogramm h)]:
 E_HFS_87 = 4.53e-24 * ureg.J
 E_HFS_85 = 2.01e-24 * ureg.J
 
-# QZ = quadratischer Zeeman-Effekt
-E_QZ_87 = calc_quad_zeemann(g_F_87, max(B1_ges), E_HFS_87, 1)
-E_QZ_85 = calc_quad_zeemann(g_F_85, max(B2_ges), E_HFS_85, 1)
+# quadratischer Zeeman-Effekt
+E_QZ_87 = calc_quad_zeemann(g_F_87, max(B1_ges), E_HFS_87, m_f=1)
+E_QZ_85 = calc_quad_zeemann(g_F_85, max(B2_ges), E_HFS_85, m_f=1)
 
 #print("Aufspaltung durch den quadratischen Zeemann-Effekt bei höheren Magnetfeldstärken")
 print(f"87Rb: {abs(E_QZ_87)} bei {max(B1_ges):.2f}")
