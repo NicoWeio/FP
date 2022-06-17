2ea103f3593256e39147d0683ce2f249d0018d6f
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-06-17 23:40:58 +0200

    …

--- V14_Tomographie/auswertung.py
+++ V14_Tomographie/auswertung.py
@@ -10,6 +10,17 @@ ureg = pint.UnitRegistry()
 console = Console()
 
 
+def calc_I(N, T):
+    """
+    Zählrate aus Anzahl N und Zeit T.
+    Input: ohne Einheiten, Output: Liste mit Einheiten.
+    """
+    I = unp.uarray(N / T, np.sqrt(N)/T) / ureg.s
+    # N *= ureg.dimensionless  # TODO
+    # T *= ureg.s
+    return I
+
+
 def kleinsteQuadrate(y, W, A):
     temp = np.dot(np.linalg.inv(np.dot(A.T, np.dot(W, A))), A.T)
     a = np.dot(temp, np.dot(W, y))
@@ -17,29 +28,36 @@ def kleinsteQuadrate(y, W, A):
     return a, np.sqrt(np.diag(a_err))
 
 
-A = np.matrix([[0, 0, 0, 0, 0, np.sqrt(2), 0, np.sqrt(2), 0],
-               [0, 0, np.sqrt(2), 0, np.sqrt(2), 0, np.sqrt(2), 0, 0],
-               [0, np.sqrt(2), 0, np.sqrt(2), 0, 0, 0, 0, 0],
+s = np.sqrt(2)
+A = np.matrix([[0, 0, 0, 0, 0, s, 0, s, 0],
+               [0, 0, s, 0, s, 0, s, 0, 0],
+               [0, s, 0, s, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 0, 1, 1, 1],
                [0, 0, 0, 1, 1, 1, 0, 0, 0],
                [1, 1, 1, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, np.sqrt(2), 0, 0, 0, np.sqrt(2), 0],
-               [np.sqrt(2), 0, 0, 0, np.sqrt(2), 0, 0, 0, np.sqrt(2)],
-               [0, np.sqrt(2), 0, 0, 0, np.sqrt(2), 0, 0, 0],
+               [0, 0, 0, s, 0, 0, 0, s, 0],
+               [s, 0, 0, 0, s, 0, 0, 0, s],
+               [0, s, 0, 0, 0, s, 0, 0, 0],
                [1, 0, 0, 1, 0, 0, 1, 0, 0],
                [0, 1, 0, 0, 1, 0, 0, 1, 0],
                [0, 0, 1, 0, 0, 1, 0, 0, 1]])
 
-A_t = np.transpose(A)  # TODO: besser direkt A.T
+μ_LIT = {
+    'Al': 0.211,
+    'Pb': 1.419,
+    'Fe': 0.606,
+    'Messing': 0.638,
+    'Delrin': 0.121,
+}
 
 MATERIALS = [
     {
         'name': 'Alu',
-        'lit': 0.202,
+        'μ_lit': 0.202,
     },
     {
         'name': 'Blei',
-        'lit': 1.25,
+        'μ_lit': 1.25,
     },
     {
         'name': 'Unb',
@@ -49,16 +67,42 @@ MATERIALS = [
     },
 ]
 
+# TODO: █ Leermessung (Luft)
+# TODO: Spektrum plotten?
+
+
+# █ Würfel X → nur Aluminiumhülle
+# → das nennen wir I_0 (!)
+console.rule("Nullmessung")
+
+# TODO: irgendwie Orientierung richtig einlesen oder anders handhaben – Pandas?
+N_0, t_0, orient_0 = np.genfromtxt(f'dat/Nullmessung.csv', delimiter=',', skip_header=1, unpack=True)
+# TODO: Nullmessung pro Material!?
+I_0 = calc_I(N_0, t_0).mean() # TODO: hier wirklich mitteln?
+print(f"{I_0=}")
+
+
+# █ Würfel YZ
 
 for material in MATERIALS:
     console.rule(material['name'])
     N, t = np.genfromtxt(f'dat/{material["name"]}.csv', delimiter=',', skip_header=1, unpack=True)
-    I = unp.uarray(N / t, np.sqrt(N)/t) / ureg.s
+    I = calc_I(N, t)
     N *= ureg.dimensionless  # TODO
     t *= ureg.s
 
-    I_0 = ureg('184/s')  # TODO: Nullmessung pro Material
+    # I_0 = ureg('184/s')  # TODO: Nullmessung pro Material
     y = unp.log((I_0 / I).to('dimensionless').m)
+    μ_mat = np.linalg.inv(A.T @ A) @ A.T @ y
+    μ = μ_mat.mean()
+
+    # print(f'{μ_mat=}')
+    print(f"{μ=}")
+
+    if 'μ_lit' in material:
+        # TODO: korrekte Einheit von μ ist 1/cm
+        print(tools.fmt_compare_to_ref(material['μ_lit'] * ureg.dimensionless, μ))
+
 
     mu_mat = np.linalg.inv(A.T @ A) @ A.T @ y
 
