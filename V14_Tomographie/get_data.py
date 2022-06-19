c7738057937402a717c51b7ee9524783ed1d522f
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-06-19 19:51:04 +0200

    …

--- V14_Tomographie/get_data.py
+++ V14_Tomographie/get_data.py
@@ -1,10 +1,8 @@
 import requests
 
-data = {
+common_data = {
     "character": 'tab',
     "Name": '',
-    "Method": '2',
-    "Formula": 'CH2O',
     "NumAdd": '1',
     "Energies": '0.6617',
     "Output": '',
@@ -15,10 +13,27 @@ data = {
     "without": 'on',
 }
 
+# data = {
+#     **common_data,
+#     "Method": '2',
+#     "Formula": 'CH2O',
+# }
+
+data = {
+    **common_data,
+    "Method": '1',
+    "OutOpt": 'PIC',
+    'ZNum': 13, # Al (eigentlich 13!?)
+}
+
 response = requests.post('https://physics.nist.gov/cgi-bin/Xcom/data.pl', data=data)
 lines = [l.split('\t') for l in response.text.strip().split('\n')]
 
-assert len(lines) == 4
+try:
+    assert len(lines) == 4
+except AssertionError:
+    print(response.text)
+    raise
 
 names = [l1.strip() + ' ' + l2.strip() for l1, l2 in zip(lines[0], lines[1]) if l1.strip() != '']
 values = lines[3]
