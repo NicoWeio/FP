cb495b6fea4ca28a55d9d06ba9f0679d958549ba
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-06-20 12:19:09 +0200

    …

--- V14_Tomographie/auswertung2.py
+++ V14_Tomographie/auswertung2.py
@@ -33,18 +33,24 @@ def A_from_indices(all_indices):
     return np.row_stack(list(map(A_row_from_indices, all_indices)))
 
 
-def get_d_for_indices(indices):
+def d_from_indices(all_indices):
+    return tools.pintify(list(map(d_row_from_indices, all_indices)))
+
+def I_0_from_indices(all_indices):
+    return tools.pintify(list(map(I_0_row_from_indices, all_indices)))
+
+def d_row_from_indices(indices):
     if '/' in indices:
         if len(indices.split('/')) == 3:
             return 3 * d_Einzelwürfel / np.sqrt(2)
         else:
             assert len(indices.split('/')) == 2
-            return 3 * d_Einzelwürfel / np.sqrt(2)
+            return 2 * d_Einzelwürfel / np.sqrt(2)
     else:
         return 3 * d_Einzelwürfel
 
 
-def get_I_0_for_indices(indices):
+def I_0_row_from_indices(indices):
     if '/' in indices:
         if len(indices.split('/')) == 3:
             return I_0_hauptdiag
@@ -72,15 +78,33 @@ def calc_I(N, T):
 d_Einzelwürfel = ureg('1 cm')
 
 
+def analyze_homogen(dat, I_0_getter):
+    A = A_from_indices(dat['indices'])
+    # print(A.round(1))
+
+    I_0 = I_0_getter(dat['indices'])
+    d = d_from_indices(dat['indices'])
+    μ_vec = (
+        unp.log((I_0 / dat['I']).to('dimensionless').m) /
+        d
+    )
+
+    print(f'{μ_vec=}')
+    µ = abs(µ_vec).mean() # TODO: SEM
+    print(f'{μ=}')
+    return µ
+
+
 def analyze(dat, I_0_getter):
     A = A_from_indices(dat['indices'])
     # print(A.round(1))
 
     I_0 = I_0_getter(dat['indices'])
     y = unp.log((I_0 / dat['I']).to('dimensionless').m)
+    d = d_from_indices(dat['indices'])
     μ_vec = (
         np.linalg.inv(A.T @ A) @ A.T @ y /
-        get_d_for_indices(dat['indices'])
+        d
     )
 
     print(f'{μ_vec=}')
@@ -108,16 +132,17 @@ def get_data(filename):
     }
 
 
-def get_closest_material(µ):
-    μ_LIT = {
-        'Al': ureg('0.211 / cm'),
-        'Pb': ureg('1.419 / cm'),
-        'Fe': ureg('0.606 / cm'),
-        'Messing': ureg('0.638 / cm'),
-        'Delrin': ureg('0.121 / cm'),
-    }
+μ_LIT = {
+    'Al': ureg('0.211 / cm'),
+    'Pb': ureg('1.419 / cm'),
+    'Fe': ureg('0.606 / cm'),
+    'Messing': ureg('0.638 / cm'),
+    'Delrin': ureg('0.121 / cm'),
+}
 
-    diff_tuples = [(abs(µ - µ_lit_single) / µ_lit_single, name) for name, µ_lit_single in µ_LIT.items()]
+
+def get_closest_material(µ):
+    diff_tuples = [(abs(µ - µ_lit_single), name) for name, µ_lit_single in µ_LIT.items()]
     diff_tuples.sort()
     return diff_tuples[0]
 
@@ -127,10 +152,24 @@ assert dat_Nullmessung['I'].check('1/[time]')
 I_0_parallel, I_0_hauptdiag, I_0_nebendiag = dat_Nullmessung['I']
 print(f"Nullmessung: {dat_Nullmessung['I']:.2f}")
 
+WÜRFEL = [
+    {
+        'num': 2,
+        'material': 'Fe',  # Mampfzwerg, SanjoR
+    },
+]
 
-dat_Würfel2 = get_data('dat2/Würfel2.csv')
-µ = analyze(dat_Würfel2, get_I_0_for_indices)
-print(f"μ = {μ:.3f}")
-# Würfel 2 SOLL: Eisen
-mat_abw, mat = get_closest_material(µ)
-print(f"Best fit: {mat} mit Abweichung {mat_abw:.1%}")
+for würfel in WÜRFEL:
+    dat = get_data(f'dat2/Würfel{würfel["num"]}.csv')
+    µ = analyze_homogen(dat, I_0_from_indices)
+    print(f"μ = {μ:.3f}")
+    mat_abw, mat = get_closest_material(µ)
+    print(f"Best fit: {mat} mit Abweichung {mat_abw:.2f}")
+    if mat == würfel['material']:
+        print("→ wie erwartet :)")
+    else:
+        print(f"→ sollte {würfel['material']} sein :(")
+    print(
+        "Abweichung best-fit vs. µ_lit des tatsächlichen Materials:\n",
+        tools.fmt_compare_to_ref(µ, µ_LIT[würfel['material']])
+    )
