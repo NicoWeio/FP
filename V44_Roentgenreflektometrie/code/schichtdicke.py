2663fae478e2e9af8a3f36b96d3cba6c212c02a9
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-10-31 22:43:12 +0100

    …

--- V44_Roentgenreflektometrie/code/schichtdicke.py
+++ V44_Roentgenreflektometrie/code/schichtdicke.py
@@ -21,9 +21,6 @@ n1 = 1  # Luft
 n2 = 1 - d1  # Polysterol
 n3 = 1 - d2  # Silizium
 
-# # Rauigkeit
-# s1 = 7.9e-10 * ureg.m  # Polysterol -> Amplitude verkleinert bei hinteren Oszillationen
-# s2 = 5.7e-10 * ureg.m  # Silizium -> Senkung des Kurvenendes und  Amplitudenverkleinerung der Oszillationen
 
 # # Schichtdicke
 # z = 855e-10 * ureg.m  # verkleinert Oszillationswellenlänge
@@ -51,23 +48,33 @@ def calc_G(α, D, d_Strahl, α_g):
 # █ Theoriekurve: Fresnelreflektivität Si
 
 
-def parratt2(z, ai, k):
-    kz1 = k * np.sqrt(np.abs(n1**2 - np.cos(ai)**2))
-    kz2 = k * np.sqrt(np.abs(n2**2 - np.cos(ai)**2))
-    kz3 = k * np.sqrt(np.abs(n3**2 - np.cos(ai)**2))
+def calc_parratt(z, α, k, α_c_Si, ureg, rauigkeit=False):
+    # Rauigkeit
+    s1 = 7.9e-10 * ureg.m  # Polysterol -> Amplitude verkleinert bei hinteren Oszillationen
+    s2 = 5.7e-10 * ureg.m  # Silizium -> Senkung des Kurvenendes und  Amplitudenverkleinerung der Oszillationen
+
+    # ----------------------------
+
+    kz1 = k * np.sqrt(np.abs(n1**2 - np.cos(α)**2))
+    kz2 = k * np.sqrt(np.abs(n2**2 - np.cos(α)**2))
+    kz3 = k * np.sqrt(np.abs(n3**2 - np.cos(α)**2))
     #
     r12 = (kz1 - kz2) / (kz1 + kz2)
     r23 = (kz2 - kz3) / (kz2 + kz3)
     r13 = ((kz1 - kz3) / (kz1 + kz3))**2
+
+    if rauigkeit:
+        r12 *= np.exp(-2 * kz1 * kz2 * s1**2)
+        r23 *= np.exp(-2 * kz2 * kz3 * s2**2)
+        r13 *= 0  # NOTE: Hierzu hat @Mampfzwerg keine Formel
     #
     x2 = np.exp(0 - (kz2 * z) * 2j) * r23
     x1 = (r12 + x2) / (1 + r12 * x2)
     par = np.abs(x1)**2
-    # Strecke vor Beginn der Oszillationen auf 1 setzen
 
-    cutoff_i = 100  # TODO: anpassen (@Mampfzwerg: 296)
-    par[:cutoff_i] = 1
-    r13[:cutoff_i] = 1
+    # Strecke vor Beginn der Oszillationen auf 1 setzen
+    par[α < α_c_Si] = 1
+    r13[α < α_c_Si] = 1
     return par, r13
 
 
@@ -103,13 +110,23 @@ def main(name, mess_refl, mess_diff, ureg, d_Strahl, α_g):
     # print(f"d_estim_a = {d_estim_a.to('m')}")
     print(f"d_estim_b = {d_estim_b.to('m')}")
 
-    # z = 855e-10  # Parameter: Schichtdicke | verkleinert Oszillationswellenlänge
-    z = d_estim_b
-    par, r13 = parratt2(z, α.to('rad').m, k=k)
-
     # berechenbar aus δ !? (@Mampfzwerg)
     α_c_PS = ureg('0.068 °')
     α_c_Si = ureg('0.210 °')
+
+    # z = 855e-10  # Parameter: Schichtdicke | verkleinert Oszillationswellenlänge
+    z = d_estim_b
+    par, r13 = calc_parratt(z, α.to('rad').m, k=k, α_c_Si=α_c_Si, ureg=ureg, rauigkeit=True)
+
+    # passe Höhe der Theoriekurve an Messwerte an
+    # TODO: poor man's fit
+    # theory_correction_factor = np.mean(I / par)
+    # theory_correction_factor = I[peaks[-3]] / par[peaks[-3]]
+    theory_correction_factor = np.mean(I[peaks[-3:]] / par[peaks[-3:]])
+    print(f"theory_correction_factor = {theory_correction_factor}")
+    par = par * theory_correction_factor
+    r13 = r13 * theory_correction_factor
+    assert par.check('1/s'), "par hat falsche Dimension"
 
     # █ Plot
     # α_linspace = tools.linspace(*tools.bounds(α), 1000)
@@ -123,8 +140,8 @@ def main(name, mess_refl, mess_diff, ureg, d_Strahl, α_g):
         # plt2.plot(q, I_diff, fmt='-', label="Messwerte (diffuse)")
         # plt2.plot(q, I_refl, fmt='-', label="Messwerte (reflektiert; roh)")
 
-        plt2.plot(q, par * max(I), fmt='-', label="Theoriekurve (Parratt)")
-        plt2.plot(q, r13 * max(I), fmt='--', label="Theoriekurve (Fresnel)")
+        plt2.plot(q, par, fmt='-', label="Theoriekurve (Parratt)")
+        plt2.plot(q, r13, fmt='--', label="Theoriekurve (Fresnel)")
 
         plt.axvline(α_to_q(α_g, λ).to('1/m'), color='C0', linestyle='--', label="$α_g$")
         plt.axvline(α_to_q(α_c_PS, λ).to('1/m'), color='C1', linestyle='--', label="$α_c$ (PS)")
