ec20caa078e6d82112fd99cccfe6021cf74ac2b0
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-11-10 20:16:17 +0100

    read T from UXD file

--- V44_Roentgenreflektometrie/main.py
+++ V44_Roentgenreflektometrie/main.py
@@ -3,6 +3,7 @@ import code.detektorscan as detektorscan
 import code.rockingscan as rockingscan
 import code.schichtdicke as schichtdicke
 import code.zscan as zscan
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,45 +22,54 @@ console = Console()
 SCANS = {
     '1_detektor': {
         'x_units': ureg.degree,
-        'T': ureg('1 s'),
+        # 'T': ureg('1 s'),
     },
     '2_z1': {
         'x_units': ureg.mm,
-        'T': ureg('1 s'),
+        # 'T': ureg('1 s'),
     },
     '3_x': {
         'x_units': ureg.mm,
-        'T': ureg('1 s'),
+        # 'T': ureg('1 s'),
     },
     '4_rocking1': {
         'x_units': ureg.degree,
-        'T': ureg('1 s'),
+        # 'T': ureg('1 s'),
     },
     '5_z2': {
         'x_units': ureg.mm,
-        'T': ureg('1 s'),
+        # 'T': ureg('1 s'),
     },
     '6_rocking2': {
         'x_units': ureg.degree,
-        'T': ureg('1 s'),
+        # 'T': ureg('1 s'),
     },
     '7_z3': {
         'x_units': ureg.mm,
-        'T': ureg('1 s'),
+        # 'T': ureg('1 s'),
     },
     '8_reflektivitaet': {
         'x_units': ureg.degree,
-        'T': ureg('5 s'),
+        # 'T': ureg('5 s'),
     },
     '9_diffus': {
         'x_units': ureg.degree,
-        'T': ureg('5 s'),
+        # 'T': ureg('5 s'),
     },
 }
 
 
-def load_scan(name, x_units, T):
-    x, N = np.genfromtxt(f"data/{name}.txt", unpack=True)  # skip_header=1
+def load_scan(name, x_units):
+    # data_path = Path(f"data_Mampfzwerg/{name}.txt") # skip_header=1
+    data_path = Path(f"data/{name}.UXD")
+
+    raw_data = data_path.read_text()
+    T_line = raw_data.splitlines()[28]
+    assert T_line.startswith('_STEPTIME=')
+    T = float(T_line.split('=')[1].strip()) * ureg.s
+    print(f"T: {T}")
+
+    x, N = np.genfromtxt(str(data_path), unpack=True, skip_header=56)
 
     # Poisson-Fehler
     N = unp.uarray(N, np.sqrt(N))
