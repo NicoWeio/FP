af399988d00f9f77e4865f99d1db8492a8b4c52f
katpopp <katharina.popp@tu-dortmund.de>
2022-12-09 00:22:59 +0100

    Abgabefertig?

--- V46_Faraday_Effekt/2_rotation.py
+++ V46_Faraday_Effekt/2_rotation.py
@@ -4,6 +4,7 @@ from scipy import optimize
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
+import scipy.constants as const
 
 # Messwerte der höher-dotierten Probe
 lamb, grad1, gradminute1, grad2, gradminute2 = np.genfromtxt('data/probe_1.csv', unpack=True)
@@ -54,27 +55,82 @@ theta_c_normiert = theta_c/L3
 print(f'Theta Probe 3 normiert: {np.round(theta_c_normiert,2)}')
 
 #Plotten der Messwerte aller drei Proben
-plt.plot(lamb**2, theta_a, 'x', label=r'Messwerte $N=\SI[per-mode=reciprocal]{2.8e18}{\per\cubic\centi\meter}$')
-plt.plot(lamb**2, theta_b, 'x', label=r'Messwerte $N=\SI[per-mode=reciprocal]{1.2e18}{\per\cubic\centi\meter}$')
-plt.plot(lamb**2, theta_c, 'x', label=r'Messwerte rein')
-plt.xlabel(r'$\lambda^2/\si{\micro\meter}$')
-plt.ylabel(r'$\theta/\si{\radian}$')
+#lamb = lamb * 10**(-6)
+
+plt.plot(lamb**2, theta_a, 'x', label=r'Messwerte $N=\SI[per-mode=reciprocal]{2.8e18}{\per\cubic\centi\meter}$', color='blue')
+plt.plot(lamb**2, theta_b, 'x', label=r'Messwerte $N=\SI[per-mode=reciprocal]{1.2e18}{\per\cubic\centi\meter}$', color='orange')
+plt.plot(lamb**2, theta_c, 'x', label=r'Messwerte rein', color='limegreen')
+plt.xlabel(r'$\lambda^2\mathbin{/}\si{\micro\meter\squared}$')
+plt.ylabel(r'$\theta\mathbin{/}\si{\radian}$')
 plt.grid()
 plt.legend()
 plt.tight_layout()
 
 plt.savefig('build/plt/rotation.pdf')
+plt.close()
 
-# Bestimmung der effektiven Masse
+# Fit der Theta-Differenz
 
 theta_norm_diff1 = theta_a_normiert - theta_c_normiert
 theta_norm_diff2 = theta_b_normiert - theta_c_normiert
+
+#u = np.linspace(np.min(df2["Filter [mikro m]"] ** 2), np.max(df2["Filter [mikro m]"] ** 2))
 
 def theta_fit(a, x):
     return a * x
 
 # Probe 1
-#param, cov1 = curve_fit(theta_fit, lamb**2, theta_norm_diff1)
-#err1 = np.sqrt(np.diag(cov1))
-#
-#print('a_1 = {:.3} $\pm$ {:.4}'.format(param[0], err1[0]))
+param1, cov1 = curve_fit(theta_fit, lamb**2, theta_norm_diff1)
+err1 = np.sqrt(np.diag(cov1))
+# Probe 2
+param2, cov2 = curve_fit(theta_fit, lamb**2, theta_norm_diff2)
+err2 = np.sqrt(np.diag(cov2))
+
+print('a_1 = {:.3} $\pm$ {:.4}'.format(param1[0], err1[0]))
+print('a_2 = {:.3} $\pm$ {:.4}'.format(param2[0], err2[0]))
+
+plt.plot(lamb**2, theta_norm_diff1, 'x', label=r'Probe $N=\SI[per-mode=reciprocal]{2.8e18}{\per\cubic\centi\meter}$', color='blue')
+plt.plot(lamb**2, theta_norm_diff2, 'x', label=r'Probe $N=\SI[per-mode=reciprocal]{1.2e18}{\per\cubic\centi\meter}$', color='orange')
+plt.plot(lamb**2, theta_fit(param1[0], lamb**2), label=r'Fit Differenz Probe $N=\SI[per-mode=reciprocal]{2.8e18}{\per\cubic\centi\meter}$', color='blue')
+plt.plot(lamb**2, theta_fit(param2[0], lamb**2), label=r'Fit Differenz Probe $N=\SI[per-mode=reciprocal]{2.8e18}{\per\cubic\centi\meter}$', color='orange')
+plt.xlabel(r'$\lambda^2\mathbin{/}\si{\micro\meter\squared}$')
+plt.ylabel(r'$\symup{\Delta}\theta\mathbin{/}\si{\radian\per\meter}')
+plt.grid()
+plt.legend()
+plt.tight_layout()
+
+plt.savefig('build/plt/fit_rotation.pdf')
+
+# Berechnung der effektiven Masse
+
+eps_0 = const.epsilon_0
+N1 = 2.8e18#*(1/100)**(-3)
+N2 = 1.2e18#*(1/100)**(-3)
+e0 = const.elementary_charge
+e_m = const.electron_mass
+B = 405e-3
+c = const.speed_of_light
+a_1 = ufloat(param1[0], cov1)*1e6
+a_2 = ufloat(param2[0], cov2)*1e6
+
+lamb_mean = np.mean(lamb)
+print(f'lambda Mittelwert={lamb_mean}')
+
+# lamb_mean = 1.904
+n = 3.4724 #bei lambda = 1.078 micrometer
+
+#m1 = unp.sqrt(e0**3 * B * N1 / (8 * np.pi**2 * eps_0 * c**3 * a_1 * n)) / e_m
+m_1 = unp.sqrt((e0**3 / (8 * np.pi**2 * eps_0 * c**3)) * 1/a_1 * (N1*B/n)) / e_m
+m_2 = unp.sqrt((e0**3 / (8 * np.pi**2 * eps_0 * c**3)) * 1/a_2 * (N2*B/n)) / e_m
+print(f'Effektive Masse Probe 1: {m_1}')
+print(f'Effektive Masse Probe 2: {m_2}')
+
+# Abweichung berechnen
+
+m_theorie = 0.067
+
+abw_1 = np.abs(m_1 - m_theorie) / m_theorie * 100
+abw_2 = np.abs(m_2 - m_theorie) / m_theorie * 100
+
+print(f'Abweichung Probe 1 in Prozent: {abw_1}')
+print(f'Abweichung Probe 2 in Prozent: {abw_2}')
